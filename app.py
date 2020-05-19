5d6500bdf5d13c176bd3219dd300a8a9e102d3a6
Saral Uttamani <saralutt@yahoo.co.in>
2020-05-19 19:21:21 -0400

    Malaria map working

--- app.py
+++ app.py
@@ -4,19 +4,6 @@ import json
 
 
 
-read_malaria_df=pd.read_csv('static/data/death-rates-malaria.csv')
-
-malaria_df=read_malaria_df.copy()
-# print (malaria_df)
-
-malaria_df = (malaria_df.groupby(['Entity']).mean())
-
-malaria_df.drop(columns=['Year'],inplace=True)
-
-
-
-malaria_json=json.loads(malaria_df.to_json())['Deaths - Malaria - Sex: Both - Age: Age-standardized (Rate) (deaths per 100,000 individuals)']
-
 read_covid_death_timeline = pd.read_csv('static/data/time_series_covid_19_deaths.csv')
 covid_death_df = read_covid_death_timeline.copy()
 
@@ -31,7 +18,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return render_template("index.html",malaria_json = malaria_json,covid_json = covid_json)
+    return render_template("index.html",covid_json = covid_json)
 
 
 if __name__ == "__main__":
--- static/main.js
+++ static/main.js
@@ -4,23 +4,26 @@ var svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height");
 
+var tooltip = d3.select('body').append('div')
+    .attr('class', 'hidden tooltip');
+
 // Map and projection
 var path = d3.geoPath();
 var projection = d3.geoMercator()
-    .scale(70)
-    .center([0, 20])
+    .scale(150)
+    .center([0, 5])
     .translate([width / 2, height / 2]);
 
 // Data and color scale
 var data = d3.map();
 var colorScale = d3.scaleThreshold()
-    .domain([100000, 1000000, 10000000, 30000000, 100000000, 500000000])
-    .range(d3.schemeBlues[7]);
+    .domain([0,2,5,7,14,15,20,25,50,75,100,120])
+    .range(d3.schemeGreens[9]);
 
 // Load external data and boot
 d3.queue()
     .defer(d3.json, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
-    .defer(d3.csv, "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world_population.csv", function (d) { data.set(d.code, +d.pop); })
+    .defer(d3.csv, "static/data/malaria_deaths.csv", function (d) { data.set(d.Entity, +d.Deaths); })
     .await(ready);
 
 function ready(error, topo) {
@@ -37,7 +40,18 @@ function ready(error, topo) {
         )
         // set the color of each country
         .attr("fill", function (d) {
-            d.total = data.get(d.id) || 0;
+            d.total = data.get(d.properties.name) || 0;
             return colorScale(d.total);
-        });
+        }).on('mousemove', function (d) {
+            var mouse = d3.mouse(svg.node()).map(function (d) {
+                return parseInt(d);
+            });
+            tooltip.classed('hidden', false)
+                .attr('style', 'left:' + (mouse[0] +100) +
+                    'px; top:' + (mouse[1] +20) + 'px')
+                .html(d.properties.name + " <br />Deaths:" + (Math.round(data.get(d.properties.name) * 100) / 100||0));
+        })
+        .on('mouseout', function () {
+            tooltip.classed('hidden', true);
+        }).attr("class","province");
 }
