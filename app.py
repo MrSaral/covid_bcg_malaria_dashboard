bd255a3d8dea6e92155f121790ab2cb7c0a05fe1
Saral Uttamani <saralutt@yahoo.co.in>
2020-05-19 17:23:47 -0400

    Map code added

--- app.py
+++ app.py
@@ -17,6 +17,13 @@ malaria_df.drop(columns=['Year'],inplace=True)
 
 malaria_json=json.loads(malaria_df.to_json())['Deaths - Malaria - Sex: Both - Age: Age-standardized (Rate) (deaths per 100,000 individuals)']
 
+read_covid_death_timeline = pd.read_csv('static/data/time_series_covid_19_deaths.csv')
+covid_death_df = read_covid_death_timeline.copy()
+
+covid_json = json.loads(covid_death_df.groupby(['Country/Region']).sum().iloc[:,[-1]].to_json())['5/18/20']
+
+# print (covid_json)
+
 
 
 app = Flask(__name__)
@@ -24,7 +31,7 @@ app = Flask(__name__)
 @app.route("/")
 
 def home():
-    return render_template("index.html",malaria_json=malaria_json)
+    return render_template("index.html",malaria_json = malaria_json,covid_json = covid_json)
 
 
 if __name__ == "__main__":
